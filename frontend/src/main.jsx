6ac24cadb6426b54cc4239d4f106a34312a1a56e
azeem2504 <118335209+azeem2504@users.noreply.github.com>
2024-12-01 00:39:26 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,14 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Analytics from '@vercel/analytics';
+import {Analytics} from '@vercel/analytics/react';
 
 
 
-Analytics();
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
